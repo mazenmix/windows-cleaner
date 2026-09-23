a2906f0631da9dcfc05d6e23c6d0f8ca8986e271
Mazen <mazenmix@gmail.com>
2026-09-23 11:38:18 +0300

    Bump MX Field Tracker to v1.1.0

--- mx-field-tracker-android/app/build.gradle.kts
+++ mx-field-tracker-android/app/build.gradle.kts
@@ -2,17 +2,35 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.mazenmix.mxfieldtracker"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "com.mazenmix.mxfieldtracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
-    buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
-    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = "17" }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
